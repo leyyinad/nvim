27b29d6befca360b0409ea75013ba620938b131c
Daniel Haus <d@danielhaus.de>
2024-03-29 11:49:25 +0100

    fix: fix commenting in mixed environments

--- lua/dvim/modules/comment/init.lua
+++ lua/dvim/modules/comment/init.lua
@@ -3,8 +3,8 @@ if not comment_status_ok then
   return
 end
 
-
 comment.setup {
+  pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
   toggler = {
     line = "<C-_><C-_>",
     block = "<C-_><C-b>",
