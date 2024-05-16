b07b1020271e8f24426baa424f6b12a0ba3dcfdb
Daniel Haus <d@danielhaus.de>
2024-05-16 08:18:44 +0200

    feat(php): add stubs refactor(php): define intelephense storage path

--- lua/dvim/modules/lsp/lang/php.lua
+++ lua/dvim/modules/lsp/lang/php.lua
@@ -1,5 +1,73 @@
 local lspconfig = require("lspconfig")
 
-lspconfig.intelephense.setup {}
-lspconfig.psalm.setup {}
+lspconfig.intelephense.setup {
+  init_options = {
+    globalStoragePath = os.getenv('HOME') .. '/.local/share/intelephense'
+  },
+  settings = {
+    intelephense = {
+      stubs = {
+        "bcmath",
+        "bz2",
+        "calendar",
+        "Core",
+        "curl",
+        "date",
+        "dba",
+        "dom",
+        "enchant",
+        "fileinfo",
+        "filter",
+        "ftp",
+        "gd",
+        "gettext",
+        "hash",
+        "iconv",
+        "imap",
+        "intl",
+        "json",
+        "ldap",
+        "libxml",
+        "mbstring",
+        "mcrypt",
+        "mysql",
+        "mysqli",
+        "password",
+        "pcntl",
+        "pcre",
+        "PDO",
+        "pdo_mysql",
+        "Phar",
+        "readline",
+        "recode",
+        "Reflection",
+        "regex",
+        "session",
+        "SimpleXML",
+        "soap",
+        "sockets",
+        "sodium",
+        "SPL",
+        "standard",
+        "superglobals",
+        "sysvsem",
+        "sysvshm",
+        "tokenizer",
+        "xml",
+        "xdebug",
+        "xmlreader",
+        "xmlwriter",
+        "yaml",
+        "zip",
+        "zlib",
+        "acf-pro",
+        "wordpress-globals",
+        "wp-cli",
+        "genesis",
+        "polylang"
+      }
+    }
+  }
+}
 
+lspconfig.psalm.setup {}
