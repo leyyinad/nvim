d746fcdefd34797aa4e76a55f8180123b4f08608
Daniel Haus <d@danielhaus.de>
2024-06-19 11:56:25 +0200

    feat(gdscript): set coding style

--- lua/dvim/modules/lsp/lang/godot.lua
+++ lua/dvim/modules/lsp/lang/godot.lua
@@ -1,4 +1,15 @@
 local lspconfig = require("lspconfig")
 
-lspconfig.gdscript.setup { }
-lspconfig.gdshader_lsp.setup { }
+lspconfig.gdscript.setup {}
+lspconfig.gdshader_lsp.setup {}
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "gdscript",
+  callback = function()
+    vim.opt_local.tabstop = 4
+    vim.opt_local.softtabstop = 4
+    vim.opt_local.shiftwidth = 4
+    vim.opt_local.autoindent = true
+    vim.opt_local.expandtab = false
+  end
+})
