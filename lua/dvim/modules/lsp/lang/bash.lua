4bd180bfd445d2f34b52f6339fe02a51517c3e64
Daniel Haus <d@danielhaus.de>
2024-09-06 11:20:42 +0200

    feat: configure .env.* ft

--- lua/dvim/modules/lsp/lang/bash.lua
+++ lua/dvim/modules/lsp/lang/bash.lua
@@ -1,3 +1,10 @@
 local lspconfig = require("lspconfig")
 
 lspconfig.bashls.setup {}
+
+vim.api.nvim_create_autocmd({ 'BufRead', 'BufNewFile' }, {
+  pattern = ".env.*",
+  callback = function()
+    vim.bo.filetype = 'config'
+  end
+})
