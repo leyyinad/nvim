96fa03cea8203182fda1e9f55c796d9a078c8fc1
Daniel Haus <d@danielhaus.de>
2024-09-06 11:20:12 +0200

    refactor: use ts_ls instead of tsserver

--- lua/dvim/modules/lsp/lang/typescript.lua
+++ lua/dvim/modules/lsp/lang/typescript.lua
@@ -1,3 +1,29 @@
 local lspconfig = require("lspconfig")
 
-lspconfig.tsserver.setup {}
+lspconfig.ts_ls.setup {}
+
+-- auto-remove unused imports when saving
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = { "*.tsx", "*.ts" },
+  callback = function()
+    vim.lsp.buf.code_action({
+      apply = true,
+      context = {
+        -- only = { "source.removeUnusedImports.ts" },
+        only = { "source.removeUnusedImports" },
+        diagnostics = {},
+      },
+    })
+
+    vim.lsp.buf.code_action({
+      apply = true,
+      context = {
+        -- only = { "source.organizeImports.ts" },
+        only = { "source.organizeImports" },
+        diagnostics = {},
+      },
+    })
+
+    vim.lsp.buf.format {}
+  end,
+})
