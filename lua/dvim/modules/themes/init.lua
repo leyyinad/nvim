040f3ecd06ae02124b9fb17d6c2bab6b19f265fd
Daniel Haus <d@danielhaus.de>
2024-09-11 13:42:09 +0200

    refactor: switch theme

--- lua/dvim/modules/themes/init.lua
+++ lua/dvim/modules/themes/init.lua
@@ -1,4 +1,23 @@
--- require("dvim.modules.themes.catppuccin")
+require("dvim.modules.themes.catppuccin")
 
-vim.cmd.colorscheme("tokyonight-night")
--- vim.cmd.colorscheme("catppuccin")
+-- vim.cmd.colorscheme("xcodedarkhc")
+
+-- material
+-- vim.g.material_terminal_italics = 1
+
+-- vim.g.material_theme_style = "darker"
+-- vim.g.material_theme_style = "palenight"
+-- vim.g.material_theme_style = "default"
+
+-- vim.g.material_theme_style = "darker-community"
+
+-- vim.g.material_theme_style = "darker"
+-- vim.g.material_theme_style = "ocean"
+
+-- vim.g.material_terminal_italics = 1
+-- -- vim.g.material_theme_style = "darker"
+-- vim.g.material_theme_style = "darker-community"
+-- vim.cmd.colorscheme("material")
+
+vim.cmd.colorscheme("catppuccin")
+-- vim.cmd.colorscheme("tokyonight-night")
