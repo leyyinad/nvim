e4f2d4fa3aea5d6e7b23054de58ba44b92c74e63
Daniel Haus <d@danielhaus.de>
2024-09-11 13:44:03 +0200

    style: reformat

--- lua/dvim/modules/netrw/init.lua
+++ lua/dvim/modules/netrw/init.lua
@@ -3,8 +3,8 @@ vim.keymap.set("", "-", ":Explore<cr>", {})
 local ghregex = [[\(^\|\s\s\)\zs\.\S\+]]
 
 vim.g.netrw_list_hide =
-  vim.fn['netrw_gitignore#Hide']() ..
-  ghregex
+    vim.fn['netrw_gitignore#Hide']() ..
+    ghregex
 
 vim.g.netrw_banner = 0
 vim.g.netrw_keepdir = 1
