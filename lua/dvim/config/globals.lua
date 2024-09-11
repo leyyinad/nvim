867ae4ce8cbef0f96dd3f3baa921eb1d896f5e24
Daniel Haus <d@danielhaus.de>
2024-09-11 13:41:35 +0200

    refactor: cleanup

--- lua/dvim/config/globals.lua
+++ lua/dvim/config/globals.lua
@@ -1,10 +1,4 @@
 vim.g.mapleader = ","
 
-vim.g.minimap_width = 10
-vim.g.minimap_auto_start = 1
-vim.g.minimap_auto_win_enter = 1
-
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
-
-vim.g.godot_executable = '/Applications/Godot.app'
