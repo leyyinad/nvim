38d4409be0342ffda0e4ac6bf3ef1e4c598a1227
Daniel Haus <d@danielhaus.de>
2024-10-11 09:03:02 +0200

    refactor: adjust godot config

--- lua/dvim/config/filetypes.lua
+++ lua/dvim/config/filetypes.lua
@@ -1,6 +1,8 @@
 vim.filetype.add({
   extension = {
-    tscn = "dosini",
-    tres = "dosini",
+    tscn = 'dosini',
+    tres = 'dosini',
+    godot = 'dosini',
+    gd = 'gdscript',
   }
 })
--- lua/dvim/modules/lsp/lang/godot.lua
+++ lua/dvim/modules/lsp/lang/godot.lua
@@ -3,8 +3,10 @@ local lspconfig = require("lspconfig")
 lspconfig.gdscript.setup {}
 lspconfig.gdshader_lsp.setup {}
 
+-- util.root_pattern("project.godot", ".git")
+
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = "gdscript",
+  pattern = { "gd", "gdscript", "gdscript3" },
   callback = function()
     vim.opt_local.tabstop = 4
     vim.opt_local.softtabstop = 4
@@ -12,4 +14,28 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.opt_local.autoindent = true
     vim.opt_local.expandtab = false
   end
+})
+
+-- auto-remove unused imports when saving
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = { "*.gd" },
+  callback = function()
+    vim.lsp.buf.code_action({
+      apply = true,
+      context = {
+        only = { "source.removeUnusedImports" },
+        diagnostics = {},
+      },
+    })
+
+    vim.lsp.buf.code_action({
+      apply = true,
+      context = {
+        only = { "source.organizeImports" },
+        diagnostics = {},
+      },
+    })
+
+    vim.lsp.buf.format {}
+  end,
 })
