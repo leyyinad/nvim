460b86e8bd5b728d3ca37b3febe6ae575b88f3e5
Daniel Haus <d@danielhaus.de>
2024-12-17 10:29:04 +0100

    feat: add git commit completion

--- lua/dvim/lazy/plugins_init.lua
+++ lua/dvim/lazy/plugins_init.lua
@@ -8,6 +8,103 @@ require("mason-lspconfig").setup()
 require("mason-nvim-dap").setup()
 require("mason-tool-installer").setup({})
 
+local cmp = require("cmp")
+
+cmp.setup({
+	snippet = {
+		-- REQUIRED - you must specify a snippet engine
+		expand = function(args)
+			-- if args.body and args.body ~= "" then
+			vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+			-- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+			-- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+			-- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+			-- vim.snippet.expand(args.body) -- For native neovim snippets (Neovim v0.10+)
+			-- end
+		end,
+	},
+	-- completion = {
+	-- 	autocomplete = {
+	-- 		cmp.TriggerEvent.TextChanged,
+	-- 		cmp.TriggerEvent.InsertEnter,
+	-- 	},
+	-- 	completeopt = "menuone,noinsert,noselect",
+	-- 	keyword_length = 0,
+	-- },
+	window = {
+		-- completion = cmp.config.window.bordered(),
+		-- documentation = cmp.config.window.bordered(),
+	},
+	mapping = cmp.mapping.preset.insert({
+		["<C-b>"] = cmp.mapping.scroll_docs(-4),
+		["<C-f>"] = cmp.mapping.scroll_docs(4),
+		["<C-Space>"] = cmp.mapping.complete(),
+		["<C-e>"] = cmp.mapping.abort(),
+		["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+	}),
+	sources = cmp.config.sources({
+		{ name = "nvim_lsp" },
+		{ name = "vsnip" }, -- For vsnip users.
+		-- { name = 'luasnip' }, -- For luasnip users.
+		-- { name = 'ultisnips' }, -- For ultisnips users.
+		-- { name = 'snippy' }, -- For snippy users.
+	}, {
+		{ name = "buffer" },
+	}),
+	formatting = {
+		expandable_indicator = true,
+		fields = { "abbr", "kind", "menu" },
+		format = function(entry, item)
+			local color_item = require("nvim-highlight-colors").format(entry, { kind = item.kind })
+
+			item = require("lspkind").cmp_format({
+				mode = "symbol", -- show only symbol annotations
+				maxwidth = {
+					-- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+					-- can also be a function to dynamically calculate max width such as
+					-- menu = function() return math.floor(0.45 * vim.o.columns) end,
+					menu = 50, -- leading text (labelDetails)
+					abbr = 50, -- actual suggestion item
+				},
+				ellipsis_char = "…", -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+				show_labelDetails = true, -- show labelDetails in menu. Disabled by default
+
+				-- The function below will be called before any actual modifications from lspkind
+				-- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
+
+				before = function(entry2, vim_item)
+					if entry2.completion_item.detail ~= nil and entry2.completion_item.detail ~= "" then
+						vim_item.menu = entry2.completion_item.detail
+					else
+						vim_item.menu = ({
+							nvim_lsp = "[LSP]",
+							luasnip = "[Snippet]",
+							buffer = "[Buffer]",
+							path = "[Path]",
+						})[entry2.source.name]
+					end
+
+					return vim_item
+				end,
+			})(entry, item)
+			if color_item.abbr_hl_group then
+				item.kind_hl_group = color_item.abbr_hl_group
+				item.kind = color_item.abbr
+			end
+			return item
+		end,
+	},
+})
+
+cmp.setup.filetype('gitcommit', {
+    sources = cmp.config.sources({
+      { name = 'git' },
+    }, {
+      { name = 'buffer' },
+    })
+ })
+ require("cmp_git").setup()
+
 -- TODO: extract modules
 
 ------------------------
@@ -108,83 +205,5 @@ require("hurl").setup()
 
 require("gdscript_extended").setup()
 require("textcase").setup()
-
-local cmp = require("cmp")
-
-cmp.setup({
-	snippet = {
-		-- REQUIRED - you must specify a snippet engine
-		expand = function(args)
-			vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-			-- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-			-- require('snippy').expand_snippet(args.body) -- For `snippy` users.
-			-- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
-			-- vim.snippet.expand(args.body) -- For native neovim snippets (Neovim v0.10+)
-		end,
-	},
-	window = {
-		-- completion = cmp.config.window.bordered(),
-		-- documentation = cmp.config.window.bordered(),
-	},
-	mapping = cmp.mapping.preset.insert({
-		["<C-b>"] = cmp.mapping.scroll_docs(-4),
-		["<C-f>"] = cmp.mapping.scroll_docs(4),
-		["<C-Space>"] = cmp.mapping.complete(),
-		["<C-e>"] = cmp.mapping.abort(),
-		["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-	}),
-	sources = cmp.config.sources({
-		{ name = "nvim_lsp" },
-		{ name = "vsnip" }, -- For vsnip users.
-		-- { name = 'luasnip' }, -- For luasnip users.
-		-- { name = 'ultisnips' }, -- For ultisnips users.
-		-- { name = 'snippy' }, -- For snippy users.
-	}, {
-		{ name = "buffer" },
-	}),
-	formatting = {
-		expandable_indicator = true,
-		fields = { "abbr", "kind", "menu" },
-		format = function(entry, item)
-			local color_item = require("nvim-highlight-colors").format(entry, { kind = item.kind })
-
-			item = require("lspkind").cmp_format({
-				mode = "symbol", -- show only symbol annotations
-				maxwidth = {
-					-- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
-					-- can also be a function to dynamically calculate max width such as
-					-- menu = function() return math.floor(0.45 * vim.o.columns) end,
-					menu = 50, -- leading text (labelDetails)
-					abbr = 50, -- actual suggestion item
-				},
-				ellipsis_char = "…", -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
-				show_labelDetails = true, -- show labelDetails in menu. Disabled by default
-
-				-- The function below will be called before any actual modifications from lspkind
-				-- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
-
-				before = function(entry2, vim_item)
-					if entry2.completion_item.detail ~= nil and entry2.completion_item.detail ~= "" then
-						vim_item.menu = entry2.completion_item.detail
-					else
-						vim_item.menu = ({
-							nvim_lsp = "[LSP]",
-							luasnip = "[Snippet]",
-							buffer = "[Buffer]",
-							path = "[Path]",
-						})[entry2.source.name]
-					end
-
-					return vim_item
-				end,
-			})(entry, item)
-			if color_item.abbr_hl_group then
-				item.kind_hl_group = color_item.abbr_hl_group
-				item.kind = color_item.abbr
-			end
-			return item
-		end,
-	},
-})
 
 require("nvim-highlight-colors").setup({})
