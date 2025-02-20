144ebe11257dbeeb8d7cb0938d00e3c8d26b80b8
Daniel Haus <d@danielhaus.de>
2025-02-20 08:25:39 +0100

    refactor: disable default schema store

--- lua/dvim/modules/lsp/lang/yaml.lua
+++ lua/dvim/modules/lsp/lang/yaml.lua
@@ -7,7 +7,16 @@ local cfg = yamlCompanion.setup({
   -- lspconfig = {
   --   cmd = {"yaml-language-server"}
   -- },
+
+  schemaStore = {
+    -- You must disable built-in schemaStore support if you want to use
+    -- this plugin and its advanced options like `ignore`.
+    enable = false,
+    -- Avoid TypeError: Cannot read properties of undefined (reading 'length')
+    url = "",
+  },
   schemas = schemastore.yaml.schemas(),
+  validate = { enable = true },
 })
 
 require("telescope").load_extension("yaml_schema")
