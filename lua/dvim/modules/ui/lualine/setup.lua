7064ab1f59f1ce6ff86afc8dae3a541c30cb472d
Daniel Haus <d@danielhaus.de>
2025-04-17 13:32:52 +0200

    feat: configure tabline and winbar

--- lua/dvim/modules/ui/lualine/setup.lua
+++ lua/dvim/modules/ui/lualine/setup.lua
@@ -3,6 +3,10 @@ if not lualine_status_ok then
   return
 end
 
+if #vim.api.nvim_list_uis() == 0 then
+  return
+end
+
 local navic_ok, navic = pcall(require, "nvim-navic")
 
 local function get_schema()
@@ -22,10 +26,64 @@ end
 
 lualine.setup({
   options = {
+    highlight = true,
+    icons_enabled = true,
+    color_correction = "static",
     section_separators = { left = "", right = "" },
     component_separators = { left = "", right = "" },
+    globalstatus = true,
   },
-  winbar = {},
+  tabline = {
+    lualine_a = {
+      {
+        "buffers",
+        hide_filename_extension = true,
+        symbols = {
+          modified = " ",
+          alternate_file = " ",
+          directory = " ",
+        },
+      },
+    },
+    lualine_b = {},
+    lualine_c = {},
+    lualine_x = { "diagnostics" },
+    lualine_y = { "branch" },
+    lualine_z = { "tabs" },
+  },
+  winbar = {
+    lualine_a = {
+      {
+        "filename",
+        path = 0,
+      },
+    },
+    lualine_b = {
+      {
+        "filetype",
+        colored = true,
+      },
+    },
+    lualine_c = {
+      {
+        function()
+          return navic_ok and navic.get_location() or ""
+        end,
+        cond = function()
+          return navic_ok and navic.is_available() or false
+        end,
+      },
+    },
+  },
+  inactive_winbar = {
+    lualine_a = {
+      {
+        "filename",
+        path = 1,
+      },
+    },
+    lualine_c = {},
+  },
   sections = {
     lualine_c = {
       {
@@ -38,5 +96,17 @@ lualine.setup({
       },
     },
     lualine_x = { "encoding", "fileformat", "filetype", get_schema },
+  },
+  inactive_sections = {
+    lualine_c = {
+      {
+        function()
+          return navic_ok and navic.get_location() or ""
+        end,
+        cond = function()
+          return navic_ok and navic.is_available() or false
+        end,
+      },
+    },
   },
 })
