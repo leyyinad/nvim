80180386a114903918bcb6c6776f9cc87673df29
Daniel Haus <d@danielhaus.de>
2025-04-17 14:21:45 +0200

    style: remove comments

--- lua/dvim/modules/themes/ayu/plugins.lua
+++ lua/dvim/modules/themes/ayu/plugins.lua
@@ -3,11 +3,5 @@ return {
     "Shatur/neovim-ayu",
     lazy = false,
     priority = 1000,
-    -- config = function()
-    --   require("ayu").setup({
-    --     transparent = false,
-    --   })
-    --   vim.cmd.colorscheme("ayu")
-    -- end,
   },
 }
