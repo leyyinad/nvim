cde9ad25c91ddc77d5f273b02807f30875ff26f4
Daniel Haus <d@danielhaus.de>
2025-04-17 10:43:39 +0200

    style: reformat

--- lua/dvim/modules/ui/lualine/plugins.lua
+++ lua/dvim/modules/ui/lualine/plugins.lua
@@ -1,3 +1,9 @@
 return {
-  { "nvim-lualine/lualine.nvim", dependencies = { "nvim-tree/nvim-web-devicons", "SmiteshP/nvim-navic" } }
+  {
+    "nvim-lualine/lualine.nvim",
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+      "SmiteshP/nvim-navic",
+    },
+  },
 }
