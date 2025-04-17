cd2da2275e129ad39a0f1b5694c13c6064577385
Daniel Haus <d@danielhaus.de>
2025-04-17 14:23:25 +0200

    style: remove comments

--- lua/dvim/modules/themes/everblush/plugins.lua
+++ lua/dvim/modules/themes/everblush/plugins.lua
@@ -3,10 +3,5 @@ return {
     "srt0/everblush.nvim",
     lazy = false,
     priority = 1000,
-    -- config = function()
-    --   require("everblush").setup({
-    --     transparent = false,
-    --   })
-    -- end,
   },
 }
