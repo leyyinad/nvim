306e3e9aa551ec985795cbc511a00d83be2f8d1c
Daniel Haus <d@danielhaus.de>
2025-04-22 09:31:52 +0200

    refactor: tweak separator color

--- lua/dvim/modules/themes/codescope/plugins.lua
+++ lua/dvim/modules/themes/codescope/plugins.lua
@@ -3,5 +3,10 @@ return {
     "srt0/codescope.nvim",
     lazy = false,
     priority = 1000,
+    config = function()
+      local colors = require("codescope.palette").colors
+
+      vim.api.nvim_set_hl(0, "WinSeparator", { fg = colors.blue })
+    end,
   },
 }
