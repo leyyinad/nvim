2da1aa585cde09f4e79f8580184e25003b473b05
Daniel Haus <d@danielhaus.de>
2025-05-08 11:49:45 +0200

    feat(ui): use ansi logo

--- lua/dvim/modules/ui/alpha/setup.lua
+++ lua/dvim/modules/ui/alpha/setup.lua
@@ -3,7 +3,26 @@ if not status_ok then
   return
 end
 
+require('alpha.term')
+
 local theta = require("alpha.themes.theta")
+local module_name = debug.getinfo(1).source
+local module_dir = string.match(module_name, "^@(.+)/")
+local assets_dir = module_dir .. "/assets"
+
+-- chafa -f symbols -O 9 -s 32x16 ~/Desktop/dh-logo.png > dh.ansi
+
+local header_ansi = {
+  type = "terminal",
+  command = "cat " .. assets_dir .. "/dh.ansi",
+  width = 32,
+  height = 14,
+  opts = {
+    redraw = true,
+    position = "center",
+    hl = "Whitespace",
+  },
+}
 
 local header = {
   type = "text",
@@ -22,7 +41,7 @@ local header = {
     [[*@@@@@       *@@@@@       @@@@@]],
     [[ @@@@@@@#+*@@@@@@@        @@@@@]],
     [[  :@@@@@@@@@@@@@%         @@@= ]],
-    [[     @@@@@@@@@:                ]]
+    [[     @@@@@@@@@:                ]],
   },
   opts = {
     position = "center",
@@ -30,13 +49,14 @@ local header = {
   },
 }
 
-alpha.setup {
+alpha.setup({
   layout = {
     { type = "padding", val = 2 },
-    header,
+    -- header,
+    header_ansi,
     { type = "padding", val = 2 },
     theta.config.layout[4],
     { type = "padding", val = 2 },
   },
   opts = theta.config.opts,
-}
+})
