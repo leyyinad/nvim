a2691c4ce4f559468d8c9b69e8fef703336ef88d
Daniel Haus <d@danielhaus.de>
2025-05-09 08:19:09 +0200

    refactor(mason): update url

--- lua/dvim/modules/packages/mason/plugins.lua
+++ lua/dvim/modules/packages/mason/plugins.lua
@@ -1,6 +1,6 @@
 return {
-  { "williamboman/mason.nvim" },
-  { "williamboman/mason-lspconfig.nvim" },
+  { "mason-org/mason.nvim" },
+  { "mason-org/mason-lspconfig.nvim" },
   { "jay-babu/mason-nvim-dap.nvim" },
   { "WhoIsSethDaniel/mason-tool-installer.nvim" },
 }
