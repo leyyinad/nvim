19d3cf04e654657f7995fb76ac18bdd93f860ce5
Daniel Haus <d@danielhaus.de>
2025-08-29 09:01:45 +0200

    refactor: split snacks config

---
+++ lua/dvim/modules/bundles/snacks/setup.lua
@@ -0,0 +1,12 @@
+local snacks = require("snacks")
+
+snacks.setup({
+	bigfile = { enabled = true },
+	indent = { enabled = true },
+	input = { enabled = true },
+	notifier = { enabled = true },
+	quickfile = { enabled = true },
+	statuscolumn = { enabled = true },
+	words = { enabled = true },
+	image = { enabled = true },
+})
