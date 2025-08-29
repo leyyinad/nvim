962f4b2bd7ede6175e1bd4b2cb8d481bcc6b7a87
Daniel Haus <d@danielhaus.de>
2025-08-29 11:50:03 +0200

    refactor: cleanup

--- lua/dvim/core/lazy/plugins.lua
+++ lua/dvim/core/lazy/plugins.lua
@@ -1,41 +1,37 @@
 local modules = require("dvim.modules")
 
-local setup = {}
-
 local module_prefix = "dvim.modules."
 local module_plugins_suffix = ".plugins"
 local module_setup_suffix = ".setup"
 
-function load_plugins()
-  local plugins = {}
+local function load_plugins()
+	local plugins = {}
 
-  for _, module_name in ipairs(modules) do
-    local module_plugins_path = module_prefix .. module_name .. module_plugins_suffix
-    local module_status_ok, module_plugins = pcall(require, module_plugins_path)
+	for _, module_name in ipairs(modules) do
+		local module_plugins_path = module_prefix .. module_name .. module_plugins_suffix
+		local module_status_ok, module_plugins = pcall(require, module_plugins_path)
 
-    if module_status_ok and type(module_plugins) == 'table' then
-      for _, plugin in ipairs(module_plugins) do
-        plugins[#plugins + 1] = plugin
-      end
-    end
-  end
+		if module_status_ok and type(module_plugins) == "table" then
+			for _, plugin in ipairs(module_plugins) do
+				plugins[#plugins + 1] = plugin
+			end
+		end
+	end
 
-  require("lazy").setup(plugins)
+	require("lazy").setup(plugins)
 end
 
-function setup_plugins()
-  local plugins = {}
+local function setup_plugins()
+	for _, module_name in ipairs(modules) do
+		local module_setup_path = module_prefix .. module_name .. module_setup_suffix
+		local module_status_ok, module_setup = pcall(require, module_setup_path)
 
-  for _, module_name in ipairs(modules) do
-    local module_setup_path = module_prefix .. module_name .. module_setup_suffix
-    local module_status_ok, module_setup = pcall(require, module_setup_path)
-
-    if not module_status_ok then
-      if package.loaded[module_setup_path] then
-        print(module_setup)
-      end
-    end
-  end
+		if not module_status_ok then
+			if package.loaded[module_setup_path] then
+				print(module_setup)
+			end
+		end
+	end
 end
 
 load_plugins()
