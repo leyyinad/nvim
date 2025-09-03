33c88961bfbc9bbe4f9563921136fd24da09b1f2
Daniel Haus <d@danielhaus.de>
2025-09-03 14:01:07 +0200

    refactor: remove unused dependency

--- lua/dvim/modules/ui/lualine/plugins.lua
+++ lua/dvim/modules/ui/lualine/plugins.lua
@@ -1,10 +1,9 @@
 return {
-	---@type LazySpec
+  ---@type LazySpec
   {
     "nvim-lualine/lualine.nvim",
     dependencies = {
       "nvim-tree/nvim-web-devicons",
-      "SmiteshP/nvim-navic",
     },
   },
 }
