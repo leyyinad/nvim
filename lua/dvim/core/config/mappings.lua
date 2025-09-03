a032364a61f0e7376283fae0d3997788fd935899
Daniel Haus <d@danielhaus.de>
2025-09-03 13:53:54 +0200

    feat: add jump in new tab mapping

--- lua/dvim/core/config/mappings.lua
+++ lua/dvim/core/config/mappings.lua
@@ -1,6 +1,9 @@
 vim.keymap.set("", "<C-ü>", "<C-]>", {})
 vim.keymap.set("n", "ü", "<C-]>", {})
 
+-- TODO: prevent `:tabe %` scrolling to top, then remove <C-o> below
+vim.keymap.set("n", "<C-t>ü", ":tabe %<CR><C-o><C-]>", {})
+
 vim.keymap.set("v", "<", "<gv", { noremap = true, desc = "Unindent last selection" })
 vim.keymap.set("v", ">", ">gv", { noremap = true, desc = "Indent last selection" })
 
