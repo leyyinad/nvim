80837d4be7a546f21766f1415a9cc17758f17713
Daniel Haus <d@danielhaus.de>
2025-09-22 20:22:18 +0200

    refactor: add type annotation

--- lua/dvim/modules/lang/lsp/plugins.lua
+++ lua/dvim/modules/lang/lsp/plugins.lua
@@ -1,19 +1,20 @@
 return {
-  {
-    "neovim/nvim-lspconfig",
-    dependencies = { "hrsh7th/cmp-nvim-lsp", "hrsh7th/nvim-cmp" },
+	---@type LazySpec
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = { "hrsh7th/cmp-nvim-lsp", "hrsh7th/nvim-cmp" },
 
-    -- config = function(_, opts)
-    --   local lspconfig = require("lspconfig")
-    --   local lsp_capabilities = require("cmp_nvim_lsp").default_capabilities()
-    --
-    --   for server, config in pairs(opts.servers) do
-    --     -- passing config.capabilities to blink.cmp merges with the capabilities in your
-    --     -- `opts[server].capabilities, if you've defined it
-    --     -- config.capabilities = require("blink.cmp").get_lsp_capabilities(config.capabilities)
-    --     config.capabilities = lsp_capabilities
-    --     lspconfig[server].setup(config)
-    --   end
-    -- end,
-  },
+		-- config = function(_, opts)
+		--   local lspconfig = require("lspconfig")
+		--   local lsp_capabilities = require("cmp_nvim_lsp").default_capabilities()
+		--
+		--   for server, config in pairs(opts.servers) do
+		--     -- passing config.capabilities to blink.cmp merges with the capabilities in your
+		--     -- `opts[server].capabilities, if you've defined it
+		--     -- config.capabilities = require("blink.cmp").get_lsp_capabilities(config.capabilities)
+		--     config.capabilities = lsp_capabilities
+		--     lspconfig[server].setup(config)
+		--   end
+		-- end,
+	},
 }
