0f683bb9a1e0ecb973ba94ffd28ab830fdc517f7
Daniel Haus <d@danielhaus.de>
2025-09-30 10:00:48 +0200

    refactor: refine godot debugging

--- lua/dvim/modules/debugging/dap/configs.lua
+++ lua/dvim/modules/debugging/dap/configs.lua
@@ -1,28 +1,29 @@
 local dap = require("dap")
 
 dap.configurations.php = {
-  {
-    type = "php",
-    name = "Launch",
-    request = "launch",
-    program = "${file}",
-    -- cwd = "${fileDirname}",
-    port = 9003,
-    runtimeArgs = {
-      "-dxdebug.start_with_request=yes",
-    },
-    env = {
-      XDEBUG_MODE = "debug,develop",
-      XDEBUG_CONFIG = "client_port=${port}",
-    },
-  },
+	{
+		type = "php",
+		name = "Launch",
+		request = "launch",
+		program = "${file}",
+		-- cwd = "${fileDirname}",
+		port = 9003,
+		runtimeArgs = {
+			"-dxdebug.start_with_request=yes",
+		},
+		env = {
+			XDEBUG_MODE = "debug,develop",
+			XDEBUG_CONFIG = "client_port=${port}",
+		},
+	},
 }
 
 dap.configurations.gdscript = {
-  {
-    type = "godot",
-    request = "launch",
-    name = "Launch scene",
-    project = "${workspaceFolder}",
-  },
+	{
+		type = "godot",
+		request = "launch",
+		name = "Launch scene",
+		project = "${workspaceFolder}",
+		launch_scene = true,
+	},
 }
