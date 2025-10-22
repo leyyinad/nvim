8f11472a77002f5269647f1a151c19a9cbff165f
Daniel Haus <d@danielhaus.de>
2025-10-22 07:57:40 +0200

    style: reformat

--- lua/dvim/modules/bundles/mini/setup.lua
+++ lua/dvim/modules/bundles/mini/setup.lua
@@ -2,7 +2,7 @@ require("mini.ai").setup()
 require("mini.align").setup()
 require("mini.animate").setup()
 require("mini.basics").setup({
-	extra_ui = true,
+  extra_ui = true,
 })
 
 require("mini.bufremove").setup()
@@ -25,46 +25,46 @@ require("mini.fuzzy").setup()
 
 local hipatterns = require("mini.hipatterns")
 hipatterns.setup({
-	highlighters = {
-		-- Highlight standalone 'FIXME', 'HACK', 'TODO', 'NOTE'
-		fixme = { pattern = "%f[%w]()FIXME()%f[%W]", group = "MiniHipatternsFixme" },
-		hack = { pattern = "%f[%w]()HACK()%f[%W]", group = "MiniHipatternsHack" },
-		todo = { pattern = "%f[%w]()TODO()%f[%W]", group = "MiniHipatternsTodo" },
-		note = { pattern = "%f[%w]()NOTE()%f[%W]", group = "MiniHipatternsNote" },
+  highlighters = {
+    -- Highlight standalone 'FIXME', 'HACK', 'TODO', 'NOTE'
+    fixme = { pattern = "%f[%w]()FIXME()%f[%W]", group = "MiniHipatternsFixme" },
+    hack = { pattern = "%f[%w]()HACK()%f[%W]", group = "MiniHipatternsHack" },
+    todo = { pattern = "%f[%w]()TODO()%f[%W]", group = "MiniHipatternsTodo" },
+    note = { pattern = "%f[%w]()NOTE()%f[%W]", group = "MiniHipatternsNote" },
 
-		-- Highlight hex color strings (`#rrggbb`) using that color
-		hex_color = hipatterns.gen_highlighter.hex_color(),
-	},
+    -- Highlight hex color strings (`#rrggbb`) using that color
+    hex_color = hipatterns.gen_highlighter.hex_color(),
+  },
 })
 
 require("mini.indentscope").setup({
-	symbol = "▏",
+  symbol = "▏",
 })
 require("mini.jump").setup()
 require("mini.jump2d").setup({
-	view = {
-		-- n_steps_ahead = 1,
-	},
-	mapping = {
-		start_jumping = "",
-	},
+  view = {
+    -- n_steps_ahead = 1,
+  },
+  mapping = {
+    start_jumping = "",
+  },
 })
 
 local MiniMap = require("mini.map")
 MiniMap.setup({
-	symbols = {
-		-- encode = nil,
-		-- encode = minimap.gen_encode_symbols.block('3x2'),
-		encode = MiniMap.gen_encode_symbols.dot("4x2"),
-		-- encode = minimap.gen_encode_symbols.shade('2x1'),
+  symbols = {
+    -- encode = nil,
+    -- encode = minimap.gen_encode_symbols.block('3x2'),
+    encode = MiniMap.gen_encode_symbols.dot("4x2"),
+    -- encode = minimap.gen_encode_symbols.shade('2x1'),
 
-		-- Scrollbar parts for view and line. Use empty string to disable any.
-		scroll_line = "█",
-		scroll_view = "┃",
-		-- - MiniMap.gen_encode_symbols.block()
-		-- - MiniMap.gen_encode_symbols.dot()
-		-- - MiniMap.gen_encode_symbols.shade()
-	},
+    -- Scrollbar parts for view and line. Use empty string to disable any.
+    scroll_line = "█",
+    scroll_view = "┃",
+    -- - MiniMap.gen_encode_symbols.block()
+    -- - MiniMap.gen_encode_symbols.dot()
+    -- - MiniMap.gen_encode_symbols.shade()
+  },
 })
 vim.keymap.set("n", "<Leader>mc", MiniMap.close)
 vim.keymap.set("n", "<Leader>mf", MiniMap.toggle_focus)
