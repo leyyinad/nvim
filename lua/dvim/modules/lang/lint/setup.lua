45716ba3c234a6a137b516559edb62d5afdde657
Daniel Haus <d@danielhaus.de>
2025-11-20 13:37:32 +0100

    feat: add php linting

--- lua/dvim/modules/lang/lint/setup.lua
+++ lua/dvim/modules/lang/lint/setup.lua
@@ -1,10 +1,17 @@
 local lint = require('lint')
 
-vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+lint.linters_by_ft = {
+  php = {
+    'php',
+    'phpcs',
+    'phpstan',
+  },
+}
+
+vim.api.nvim_create_autocmd({ "BufWritePost", "BufReadPost" }, {
   callback = function()
     -- try_lint without arguments runs the linters defined in `linters_by_ft`
     -- for the current filetype
     lint.try_lint()
   end,
 })
-
