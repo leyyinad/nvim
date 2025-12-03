3d6ed1f37a8ae45f32df5a6f77d9d1334436c84a
Daniel Haus <d@danielhaus.de>
2025-12-03 08:06:12 +0100

    style: reformat

--- lua/dvim/modules/completion/blink/plugins.lua
+++ lua/dvim/modules/completion/blink/plugins.lua
@@ -49,7 +49,10 @@ return {
       -- (Default) Only show the documentation popup when manually triggered
       -- completion = { documentation = { auto_show = false } },
       completion = {
-        documentation = { auto_show = true },
+        documentation = {
+          auto_show = true,
+        },
+
         menu = {
           draw = {
             columns = {
@@ -116,7 +119,12 @@ return {
       -- Default list of enabled providers defined so that you can extend it
       -- elsewhere in your config, without redefining it, due to `opts_extend`
       sources = {
-        default = { "lsp", "path", "snippets", "buffer" },
+        default = {
+          "lsp",
+          "path",
+          "snippets",
+          "buffer"
+        },
       },
 
       -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
