4c28571f4c2e6fec5f0488df028180ab5fb47fff
Daniel Haus <d@danielhaus.de>
2025-12-03 08:09:10 +0100

    fix: remove unsupported code actions

--- lua/dvim/modules/lang/lsp/lang/php.lua
+++ lua/dvim/modules/lang/lsp/lang/php.lua
@@ -13,85 +13,85 @@ local lspconfig = require("lspconfig")
 vim.lsp.enable("phpactor", false)
 
 vim.lsp.config("intelephense", {
-	init_options = {
-		-- storagePath = …, -- Optional absolute path to storage dir. Defaults to os.tmpdir().
-		-- licenceKey = …, -- Optional licence key or absolute path to a text file containing the licence key.
-		-- clearCache = …, -- Optional flag to clear server state. State can also be cleared by deleting {storagePath}/intelephense
+  init_options = {
+    -- storagePath = …, -- Optional absolute path to storage dir. Defaults to os.tmpdir().
+    -- licenceKey = …, -- Optional licence key or absolute path to a text file containing the licence key.
+    -- clearCache = …, -- Optional flag to clear server state. State can also be cleared by deleting {storagePath}/intelephense
 
-		-- Optional absolute path to a global storage dir. Defaults to os.homedir().
-		globalStoragePath = os.getenv("HOME") .. "/.local/share/intelephense",
-	},
-	settings = {
-		intelephense = {
-			files = {
-				-- See https://github.com/bmewburn/intelephense-docs
-				maxSize = 1000000,
-			},
-			stubs = {
-				"bcmath",
-				"bz2",
-				"calendar",
-				"Core",
-				"curl",
-				"date",
-				"dba",
-				"dom",
-				"enchant",
-				"exif",
-				"fileinfo",
-				"filter",
-				"ftp",
-				"gd",
-				"gettext",
-				"hash",
-				"iconv",
-				"imap",
-				"intl",
-				"json",
-				"ldap",
-				"libxml",
-				"mbstring",
-				"mcrypt",
-				"mysql",
-				"mysqli",
-				"password",
-				"pcntl",
-				"pcre",
-				"posix",
-				"PDO",
-				"pdo_mysql",
-				"Phar",
-				"random",
-				"readline",
-				"recode",
-				"Reflection",
-				"regex",
-				"session",
-				"SimpleXML",
-				"soap",
-				"sockets",
-				"sodium",
-				"SPL",
-				"standard",
-				"superglobals",
-				"sysvsem",
-				"sysvshm",
-				"tokenizer",
-				"xml",
-				"xdebug",
-				"xmlreader",
-				"xmlwriter",
-				"yaml",
-				"zip",
-				"zlib",
-				"acf-pro",
-				"wordpress-globals",
-				"wp-cli",
-				"genesis",
-				"polylang",
-			},
-		},
-	},
+    -- Optional absolute path to a global storage dir. Defaults to os.homedir().
+    globalStoragePath = os.getenv("HOME") .. "/.local/share/intelephense",
+  },
+  settings = {
+    intelephense = {
+      files = {
+        -- See https://github.com/bmewburn/intelephense-docs
+        maxSize = 1000000,
+      },
+      stubs = {
+        "bcmath",
+        "bz2",
+        "calendar",
+        "Core",
+        "curl",
+        "date",
+        "dba",
+        "dom",
+        "enchant",
+        "exif",
+        "fileinfo",
+        "filter",
+        "ftp",
+        "gd",
+        "gettext",
+        "hash",
+        "iconv",
+        "imap",
+        "intl",
+        "json",
+        "ldap",
+        "libxml",
+        "mbstring",
+        "mcrypt",
+        "mysql",
+        "mysqli",
+        "password",
+        "pcntl",
+        "pcre",
+        "posix",
+        "PDO",
+        "pdo_mysql",
+        "Phar",
+        "random",
+        "readline",
+        "recode",
+        "Reflection",
+        "regex",
+        "session",
+        "SimpleXML",
+        "soap",
+        "sockets",
+        "sodium",
+        "SPL",
+        "standard",
+        "superglobals",
+        "sysvsem",
+        "sysvshm",
+        "tokenizer",
+        "xml",
+        "xdebug",
+        "xmlreader",
+        "xmlwriter",
+        "yaml",
+        "zip",
+        "zlib",
+        "acf-pro",
+        "wordpress-globals",
+        "wp-cli",
+        "genesis",
+        "polylang",
+      },
+    },
+  },
 })
 
 vim.lsp.enable("intelephense")
@@ -99,24 +99,8 @@ vim.lsp.enable("intelephense")
 -- lspconfig.psalm.setup({})
 
 vim.api.nvim_create_autocmd("BufWritePre", {
-	pattern = { "*.php" },
-	callback = function()
-		vim.lsp.buf.code_action({
-			apply = true,
-			context = {
-				only = { "source.removeUnusedImports" },
-				diagnostics = {},
-			},
-		})
-
-		vim.lsp.buf.code_action({
-			apply = true,
-			context = {
-				only = { "source.organizeImports" },
-				diagnostics = {},
-			},
-		})
-
-		vim.lsp.buf.format({})
-	end,
+  pattern = { "*.php" },
+  callback = function()
+    vim.lsp.buf.format({})
+  end,
 })
