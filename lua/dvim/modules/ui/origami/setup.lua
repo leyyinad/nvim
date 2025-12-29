6129d0427a01beb57c0126731d9203411c42dedf
Daniel Haus <d@danielhaus.de>
2025-12-29 10:36:38 +0100

    fix: remove default config

--- lua/dvim/modules/ui/origami/setup.lua
+++ lua/dvim/modules/ui/origami/setup.lua
@@ -1,35 +1,14 @@
 -- default settings
 local origami = require("origami")
 
-origami.setup({
-	useLspFoldsWithTreesitterFallback = true,
-	pauseFoldsOnSearch = true,
-	foldtext = {
-		enabled = true,
-		padding = 3,
-		lineCount = {
-			template = "%d lines", -- `%d` is replaced with the number of folded lines
-			hlgroup = "Comment",
-		},
-		diagnosticsCount = true, -- uses hlgroups and icons from `vim.diagnostic.config().signs`
-		gitsignsCount = true, -- requires `gitsigns.nvim`
-	},
-	autoFold = {
-		enabled = true,
-		kinds = { "comment", "imports" }, ---@type lsp.FoldingRangeKind[]
-	},
-	foldKeymaps = {
-		setup = true, -- modifies `h`, `l`, and `$`
-		hOnlyOpensOnFirstColumn = false,
-	},
-})
+origami.setup()
 
 vim.keymap.set("n", "<Left>", function()
-	origami.h()
+  origami.h()
 end)
 vim.keymap.set("n", "<Right>", function()
-	origami.l()
+  origami.l()
 end)
 vim.keymap.set("n", "<End>", function()
-	origami.dollar()
+  origami.dollar()
 end)
