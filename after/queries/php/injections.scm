0472f1ceb09bf7b91c8602dc5020ff84152e092b
Daniel Haus <d@danielhaus.de>
2026-01-22 10:32:53 +0100

    feat(php): highlight sql in strings

---
+++ after/queries/php/injections.scm
@@ -0,0 +1,29 @@
+;; extends
+
+; ((encapsed_string (string_content) @injection.content)
+;  (#match? @injection.content
+;   "^\[\s\r]\*SELECT|select|RELACE|replace|INSERT|insert|UPDATE|update|DELETE|delete|CREATE|create|ALTER|alter|DROP|drop|EXPLAIN|explain|TRUNCATE|truncate|BEGIN|begin")
+;  (#set! injection.language "sql")
+;  (#set! injection.include-children))
+
+([
+  (string_content)
+  (heredoc_body)
+  (nowdoc_body)
+ ] @injection.content
+ (#match? @injection.content
+  "^\[\s\r]\*SELECT|select|RELACE|replace|INSERT|insert|UPDATE|update|DELETE|delete|CREATE|create|ALTER|alter|DROP|drop|EXPLAIN|explain|TRUNCATE|truncate|BEGIN|begin")
+ (#set! injection.language "sql")
+ (#set! injection.include-children))
+
+((function_call_expression
+  function: (_) @_func_identifier
+  arguments: (arguments . (argument (_ (string_content) @injection.content))))
+  (#set! injection.language "printf")
+  (#any-of? @_func_identifier "sprintf" "printf" "vprintf" "vsprintf"))
+
+((function_call_expression
+  function: (_) @_func_identifier
+  arguments: (arguments (_) . (argument (_ (string_content) @injection.content))))
+  (#set! injection.language "printf")
+  (#any-of? @_func_identifier "fprintf" "fscanf" "sscanf"))
