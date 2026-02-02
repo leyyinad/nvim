e2610b939541b199998d1650bbab42d401a42b0c
Daniel Haus <d@danielhaus.de>
2026-02-02 16:35:33 +0100

    feat: add :Dashboard command

--- lua/dvim/modules/bundles/snacks/setup.lua
+++ lua/dvim/modules/bundles/snacks/setup.lua
@@ -1,2 +1,10 @@
 require("dvim.modules.bundles.snacks.config")
 require("dvim.modules.bundles.snacks.picker")
+
+vim.api.nvim_create_user_command(
+  "Dashboard",
+  function()
+    Snacks.dashboard()
+  end,
+  {}
+)
