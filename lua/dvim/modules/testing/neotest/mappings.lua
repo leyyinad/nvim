0439a65a56163779c623ec13e49edcd2e70979fd
Daniel Haus <d@danielhaus.de>
2026-04-17 09:21:08 +0200

    fix(neotest): fix run file mapping

--- lua/dvim/modules/testing/neotest/mappings.lua
+++ lua/dvim/modules/testing/neotest/mappings.lua
@@ -1,40 +1,42 @@
+local neotest = require('neotest')
+
 vim.keymap.set("n", "<leader>ts", function()
-  require("neotest").summary.open()
+  neotest.summary.open()
 end, {
   noremap = true,
   desc = "Show test summary",
 })
 
 vim.keymap.set("n", "<leader>tr", function()
-  require("neotest").run.run()
+  neotest.run.run()
 end, {
   noremap = true,
   desc = "Run test",
 })
 
 vim.keymap.set("n", "<leader>td", function()
-  require("neotest").run.run({ strategy = "dap" })
+  neotest.run.run({ strategy = "dap" })
 end, {
   noremap = true,
   desc = "Debug test",
 })
 
 vim.keymap.set("n", "<leader>tt", function()
-  require("neotest").run.run_file()
+  neotest.run.run(vim.fn.expand("%"))
 end, {
   noremap = true,
   desc = "Run test file",
 })
 
 vim.keymap.set("n", "<leader>tl", function()
-  require("neotest").run.run_last()
+  neotest.run.run_last()
 end, {
   noremap = true,
   desc = "Run last test",
 })
 
 vim.keymap.set("n", "<leader>tL", function()
-  require("neotest").run.run_last({ strategy = "dap " })
+  neotest.run.run_last({ strategy = "dap " })
 end, {
   noremap = true,
   desc = "Debug last test",
