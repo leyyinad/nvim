5df7c207a774ddffaad18b8218672244fd0aa919
Daniel Haus <d@danielhaus.de>
2026-05-20 12:30:01 +0200

    refactor: remove log level setting

--- lua/dvim/modules/ai/codecompanion/plugins.lua
+++ lua/dvim/modules/ai/codecompanion/plugins.lua
@@ -4,13 +4,7 @@ return {
     "olimorris/codecompanion.nvim",
     dependencies = {
       "nvim-lua/plenary.nvim",
-      "nvim-treesitter/nvim-treesitter",
-    },
-    opts = {
-      -- NOTE: The log_level is in `opts.opts`
-      opts = {
-        log_level = "DEBUG", -- or "TRACE"
-      },
-    },
-  },
+      "nvim-treesitter/nvim-treesitter"
+    }
+  }
 }
