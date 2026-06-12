12ad0a97d81f6e02cdc1ccd1466afb154736c508
Daniel Haus <d@danielhaus.de>
2026-06-13 00:43:02 +0200

    refactor(blink): revert mappings

--- lua/dvim/modules/completion/blink/plugins.lua
+++ lua/dvim/modules/completion/blink/plugins.lua
@@ -36,8 +36,8 @@ return {
 			--
 			-- See :h blink-cmp-config-keymap for defining your own keymap
 			-- keymap = { preset = "default" },
-			-- keymap = { preset = "enter" },
-			keymap = { preset = "super-tab" },
+			keymap = { preset = "enter" },
+			-- keymap = { preset = "super-tab" },
 			appearance = {
 				-- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
 				-- Adjusts spacing to ensure icons are aligned
