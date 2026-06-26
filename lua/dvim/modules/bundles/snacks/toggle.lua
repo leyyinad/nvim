29b64bbc287ffdacdbcec4193981df9a22f537d9
Daniel Haus <d@danielhaus.de>
2026-06-26 10:20:59 +0200

    feat: add commands to disable autoformat feat: add autoformat toggles

--- lua/dvim/modules/bundles/snacks/toggle.lua
+++ lua/dvim/modules/bundles/snacks/toggle.lua
@@ -1,8 +1,8 @@
 Snacks.toggle.option("spell", { name = "Spelling" }):map("<leader>.s")
 Snacks.toggle.option("wrap", { name = "Wrap" }):map("<leader>.w")
-Snacks.toggle.option("relativenumber", { name = "Relative Number" }):map("<leader>.L")
+Snacks.toggle.option("relativenumber", { name = "Relative Number" }):map("<leader>.N")
 Snacks.toggle.diagnostics():map("<leader>.d")
-Snacks.toggle.line_number():map("<leader>.l")
+Snacks.toggle.line_number():map("<leader>.n")
 Snacks.toggle
 	.option("conceallevel", { off = 0, on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2 })
 	:map("<leader>.c")
@@ -10,3 +10,32 @@ Snacks.toggle.treesitter():map("<leader>.T")
 Snacks.toggle.inlay_hints():map("<leader>.h")
 Snacks.toggle.indent():map("<leader>.g")
 Snacks.toggle.dim():map("<leader>.D")
+
+function Snacks.toggle.autoformat_buffer()
+	return Snacks.toggle.new({
+		id = "autoformat_buffer",
+		name = "Auto format (buffer)",
+		get = function()
+			return not vim.b.disable_autoformat
+		end,
+		set = function(state)
+			vim.b.disable_autoformat = not state
+		end,
+	})
+end
+
+function Snacks.toggle.autoformat_global()
+	return Snacks.toggle.new({
+		id = "autoformat_global",
+		name = "Auto format (global)",
+		get = function()
+			return not vim.g.disable_autoformat
+		end,
+		set = function(state)
+			vim.g.disable_autoformat = not state
+		end,
+	})
+end
+
+Snacks.toggle.autoformat_buffer():map("<leader>.f")
+Snacks.toggle.autoformat_global():map("<leader>.F")
