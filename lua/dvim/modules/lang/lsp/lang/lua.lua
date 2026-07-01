dbc46910609852b8ffd103b64230ff704abac3ee
Daniel Haus <d@danielhaus.de>
2026-07-01 10:55:23 +0200

    refactor: use lua_ls lsp

--- lua/dvim/modules/lang/lsp/lang/lua.lua
+++ lua/dvim/modules/lang/lsp/lang/lua.lua
@@ -1,34 +1,81 @@
-vim.lsp.config("emmylua_ls", {
-	on_init = function(client)
-		-- If the workspace has its own emmylua_ls/lua_ls config file, defer to it.
-		if client.workspace_folders then
-			local path = client.workspace_folders[1].name
-			if
-				path ~= vim.fn.stdpath("config")
-				and (vim.uv.fs_stat(path .. "/.emmyrc.json") or vim.uv.fs_stat(path .. "/.luarc.json"))
-			then
-				client.config.settings = {}
-			end
-		end
-	end,
-	settings = {
-		emmylua = {
-			-- Tell the server which Lua you're using (usually LuaJIT, for Neovim).
-			runtime = { version = "LuaJIT" },
-			diagnostics = { globals = { "vim" } },
-			-- Make the server aware of Neovim runtime files.
-			workspace = {
-				library = {
-					vim.env.VIMRUNTIME,
-					-- For LSP Settings Type Annotations: https://github.com/neovim/nvim-lspconfig#lsp-settings-type-annotations
-					vim.api.nvim_get_runtime_file("lua/lspconfig", false)[1],
-				},
-				-- Or pull in all of 'runtimepath'. May be slower! https://github.com/neovim/nvim-lspconfig/issues/3189
-				-- library = vim.api.nvim_get_runtime_file("", true),
-				-- library = runtime_files,
-			},
-		},
-	},
+vim.lsp.config("lua_ls", {
+  on_init = function(client)
+    if client.workspace_folders then
+      local path = client.workspace_folders[1].name
+      if
+          path ~= vim.fn.stdpath("config")
+          and (vim.uv.fs_stat(path .. "/.luarc.json") or vim.uv.fs_stat(path .. "/.luarc.jsonc"))
+      then
+        return
+      end
+    end
+
+    client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
+      runtime = {
+        -- Tell the language server which version of Lua you're using (most
+        -- likely LuaJIT in the case of Neovim)
+        version = "LuaJIT",
+        -- Tell the language server how to find Lua modules same way as Neovim
+        -- (see `:h lua-module-load`)
+        path = {
+          "lua/?.lua",
+          "lua/?/init.lua",
+        },
+      },
+      -- Make the server aware of Neovim runtime files
+      workspace = {
+        checkThirdParty = false,
+        library = {
+          vim.env.VIMRUNTIME,
+          -- For LSP Settings Type Annotations: https://github.com/neovim/nvim-lspconfig#lsp-settings-type-annotations
+          vim.api.nvim_get_runtime_file("lua/lspconfig", false)[1],
+        },
+        -- Or pull in all of 'runtimepath'.
+        -- NOTE: this is a lot slower and will cause issues when working on
+        -- your own configuration.
+        -- See https://github.com/neovim/nvim-lspconfig/issues/3189
+        -- library = vim.api.nvim_get_runtime_file('', true),
+      },
+    })
+  end,
+  settings = {
+    Lua = {},
+  },
 })
 
-vim.lsp.enable("emmylua_ls")
+vim.lsp.enable("lua_ls")
+
+vim.lsp.config("emmylua_ls", {
+  on_init = function(client)
+    -- If the workspace has its own emmylua_ls/lua_ls config file, defer to it.
+    if client.workspace_folders then
+      local path = client.workspace_folders[1].name
+      if
+          path ~= vim.fn.stdpath("config")
+          and (vim.uv.fs_stat(path .. "/.emmyrc.json") or vim.uv.fs_stat(path .. "/.luarc.json"))
+      then
+        client.config.settings = {}
+      end
+    end
+  end,
+  settings = {
+    emmylua = {
+      -- Tell the server which Lua you're using (usually LuaJIT, for Neovim).
+      runtime = { version = "LuaJIT" },
+      diagnostics = { globals = { "vim" } },
+      -- Make the server aware of Neovim runtime files.
+      workspace = {
+        -- library = {
+        -- 	vim.env.VIMRUNTIME,
+        -- 	-- For LSP Settings Type Annotations: https://github.com/neovim/nvim-lspconfig#lsp-settings-type-annotations
+        -- 	vim.api.nvim_get_runtime_file("lua/lspconfig", false)[1],
+        -- },
+        -- Or pull in all of 'runtimepath'. May be slower! https://github.com/neovim/nvim-lspconfig/issues/3189
+        library = vim.api.nvim_get_runtime_file("", true),
+        -- library = runtime_files,
+      },
+    },
+  },
+})
+
+vim.lsp.enable("emmylua_ls", false)
