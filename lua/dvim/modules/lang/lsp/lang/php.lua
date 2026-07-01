4cf577256ef3f87cfd3897dd6021a75cf8c11616
Daniel Haus <d@danielhaus.de>
2026-07-01 11:00:25 +0200

    feat: migrate to phpantom_lsp

--- lua/dvim/modules/lang/lsp/lang/php.lua
+++ lua/dvim/modules/lang/lsp/lang/php.lua
@@ -1,6 +1,4 @@
-local lspconfig = require("lspconfig")
-
-vim.lsp.enable("phpantom_lsp", false)
+vim.lsp.enable("phpantom_lsp", true)
 
 -- lspconfig.phpactor.setup({
 -- init_options = {
@@ -14,98 +12,98 @@ vim.lsp.enable("phpantom_lsp", false)
 
 vim.lsp.enable("phpactor", false)
 
-vim.lsp.config("intelephense", {
-  init_options = {
-    -- storagePath = …, -- Optional absolute path to storage dir. Defaults to os.tmpdir().
-    -- licenceKey = …, -- Optional licence key or absolute path to a text file containing the licence key.
-    -- clearCache = …, -- Optional flag to clear server state. State can also be cleared by deleting {storagePath}/intelephense
-    -- Optional absolute path to a global storage dir. Defaults to os.homedir().
-    globalStoragePath = os.getenv("HOME") .. "/.local/share/intelephense"
-  },
-  settings = {
-    intelephense = {
-      files = {
-        -- See https://github.com/bmewburn/intelephense-docs
-        maxSize = 1000000
-      },
-      stubs = {
-        "bcmath",
-        "bz2",
-        "calendar",
-        "Core",
-        "curl",
-        "date",
-        "dba",
-        "dom",
-        "enchant",
-        "exif",
-        "fileinfo",
-        "filter",
-        "ftp",
-        "gd",
-        "gettext",
-        "hash",
-        "iconv",
-        "imap",
-        "intl",
-        "json",
-        "ldap",
-        "libxml",
-        "mbstring",
-        "mcrypt",
-        "mysql",
-        "mysqli",
-        "password",
-        "pcntl",
-        "pcre",
-        "posix",
-        "PDO",
-        "pdo_mysql",
-        "Phar",
-        "random",
-        "readline",
-        "recode",
-        "Reflection",
-        "regex",
-        "session",
-        "SimpleXML",
-        "soap",
-        "sockets",
-        "sodium",
-        "SPL",
-        "standard",
-        "superglobals",
-        "sysvsem",
-        "sysvshm",
-        "tokenizer",
-        "xml",
-        "xdebug",
-        "xmlreader",
-        "xmlwriter",
-        "yaml",
-        "zip",
-        "zlib",
-        "acf-pro",
-        "wordpress-globals",
-        "wp-cli",
-        "genesis",
-        "polylang"
-      }
-    }
-  },
-  root_markers = {
-    "composer.json",
-    ".git"
-  }
-})
+-- vim.lsp.config("intelephense", {
+-- 	init_options = {
+-- 		-- storagePath = …, -- Optional absolute path to storage dir. Defaults to os.tmpdir().
+-- 		-- licenceKey = …, -- Optional licence key or absolute path to a text file containing the licence key.
+-- 		-- clearCache = …, -- Optional flag to clear server state. State can also be cleared by deleting {storagePath}/intelephense
+-- 		-- Optional absolute path to a global storage dir. Defaults to os.homedir().
+-- 		globalStoragePath = os.getenv("HOME") .. "/.local/share/intelephense",
+-- 	},
+-- 	settings = {
+-- 		intelephense = {
+-- 			files = {
+-- 				-- See https://github.com/bmewburn/intelephense-docs
+-- 				maxSize = 1000000,
+-- 			},
+-- 			stubs = {
+-- 				"bcmath",
+-- 				"bz2",
+-- 				"calendar",
+-- 				"Core",
+-- 				"curl",
+-- 				"date",
+-- 				"dba",
+-- 				"dom",
+-- 				"enchant",
+-- 				"exif",
+-- 				"fileinfo",
+-- 				"filter",
+-- 				"ftp",
+-- 				"gd",
+-- 				"gettext",
+-- 				"hash",
+-- 				"iconv",
+-- 				"imap",
+-- 				"intl",
+-- 				"json",
+-- 				"ldap",
+-- 				"libxml",
+-- 				"mbstring",
+-- 				"mcrypt",
+-- 				"mysql",
+-- 				"mysqli",
+-- 				"password",
+-- 				"pcntl",
+-- 				"pcre",
+-- 				"posix",
+-- 				"PDO",
+-- 				"pdo_mysql",
+-- 				"Phar",
+-- 				"random",
+-- 				"readline",
+-- 				"recode",
+-- 				"Reflection",
+-- 				"regex",
+-- 				"session",
+-- 				"SimpleXML",
+-- 				"soap",
+-- 				"sockets",
+-- 				"sodium",
+-- 				"SPL",
+-- 				"standard",
+-- 				"superglobals",
+-- 				"sysvsem",
+-- 				"sysvshm",
+-- 				"tokenizer",
+-- 				"xml",
+-- 				"xdebug",
+-- 				"xmlreader",
+-- 				"xmlwriter",
+-- 				"yaml",
+-- 				"zip",
+-- 				"zlib",
+-- 				"acf-pro",
+-- 				"wordpress-globals",
+-- 				"wp-cli",
+-- 				"genesis",
+-- 				"polylang",
+-- 			},
+-- 		},
+-- 	},
+-- 	root_markers = {
+-- 		"composer.json",
+-- 		".git",
+-- 	},
+-- })
 
-vim.lsp.enable("intelephense")
+vim.lsp.enable("intelephense", false)
 
 -- lspconfig.psalm.setup({})
 
 vim.api.nvim_create_autocmd("BufWritePre", {
-  pattern = { "*.php" },
-  callback = function ()
-    vim.lsp.buf.format({})
-  end
+	pattern = { "*.php" },
+	callback = function()
+		vim.lsp.buf.format({})
+	end,
 })
