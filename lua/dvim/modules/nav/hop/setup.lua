05c3e62f6abb26ad7063feed6cf42e78f386661a
Daniel Haus <d@danielhaus.de>
2026-08-31 18:28:55 +0200

    fix(hop): restrict hop to normal buffers

--- lua/dvim/modules/nav/hop/setup.lua
+++ lua/dvim/modules/nav/hop/setup.lua
@@ -3,10 +3,23 @@ local directions = require("hop.hint").HintDirection
 
 hop.setup({})
 
-vim.keymap.set({ "n", "x", "o" }, "<CR>", function()
-  hop.hint_words({
-    direction = directions.AFTER_CURSOR | directions.BEFORE_CURSOR,
-  })
-end, {
-  remap = true,
+vim.api.nvim_create_autocmd("BufWinEnter", {
+  group = vim.api.nvim_create_augroup("CustomBufferKeymaps", { clear = true }),
+  callback = function(args)
+    local buftype = vim.bo[args.buf].buftype
+
+    if buftype ~= "" then
+      return
+    end
+
+    vim.keymap.set({ "n", "x", "o" }, "<CR>", function()
+      hop.hint_words({
+        direction = directions.AFTER_CURSOR | directions.BEFORE_CURSOR,
+      })
+    end, {
+      buffer = args.buf,
+      desc = "Hop to word",
+      silent = true,
+    })
+  end,
 })
