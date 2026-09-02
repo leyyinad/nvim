67fd94362ca90109ab03f22b1e305e0e2b0c846f
Daniel Haus <d@danielhaus.de>
2026-09-02 19:35:09 +0200

    feat(lint): add gdscript-formatter for linting

--- lua/dvim/modules/lang/lint/setup.lua
+++ lua/dvim/modules/lang/lint/setup.lua
@@ -1,10 +1,40 @@
 local lint = require("lint")
+local parser = require("lint.parser")
+
+local pattern = [[([^:]+):(%d+):([^:]+):(%a+):%s(.*)]]
+
+local groups = {
+  "file",
+  "lnum",
+  "code",
+  "severity",
+  "message",
+}
+
+local severity_map = {
+  ["error"] = vim.diagnostic.severity.WARN,
+  ["warning"] = vim.diagnostic.severity.WARN,
+}
+
+lint.linters.gdscript_formatter = {
+  name = "gdscript_formatter",
+  cmd = "gdscript-formatter",
+  stdin = false,
+  args = { "lint" },
+  -- stream = "both",
+  ignore_exitcode = true,
+  env = nil,
+  parser = parser.from_pattern(pattern, groups, severity_map, { ["source"] = "gdscript-formatter" }),
+}
 
 lint.linters_by_ft = {
   php = {
     "php",
     "phpcs",
     "phpstan",
+  },
+  gdscript = {
+    "gdscript_formatter",
   },
 }
 
